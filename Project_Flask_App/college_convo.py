7e7e20655709ddbd6dc39b01db598d65648fcc2e
melrick04 <mca.1924@unigoa.ac.in>
2021-07-06 21:15:25 +0530

    college convo

---
+++ Project_Flask_App/college_convo.py
@@ -0,0 +1,220 @@
+general_college_intents={"intents":[
+
+    {
+        "tag": "college_details",
+        "patterns": ["college details","institution","college"],
+        "responses": ["Choose your option<br><button type='button' onClick='buttonAction(this) '> General Education Colleges </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Professional Colleges </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Recognised Institutions </button>"],
+ 
+    },
+
+    {
+        "tag": "gen_education_colleges",
+        "patterns": ["education general college","affiliated educational general college","general education college"],
+        "responses": ["Click on colleges to know more: \
+        <br /> <button type='button' onClick='buttonAction(this) '> Carmel College of Arts, Science & Commerce for Women </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Cuncolim Educational Society's College of Arts & Commerce </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Dempo Charities Trust Dhempe College of Arts & Science </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Diocesan Society of Education’s Rosary College of Commerce & Arts </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Dnyan Prabodhini Mandal's Shree Mallikarjun and Shri Chetan Manju Desai College </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Fr. Agnel College of Arts & Commerce </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Goa College of Home Science </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Goa College of Hospitality & Culinary Education </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Government College of Arts, Science & Commerce, Khandolas </button>"],
+ 
+    },
+
+    {
+        "tag": "professional_colleges",
+        "patterns": ["college professional","college affiliated professional","professional college"],
+        "responses": ["Click on colleges to know more: \
+        <br /> <button type='button' onClick='buttonAction(this) '> Agnel Institute of Technology and Design </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Bharateeya Sanskriti Prabodhini's Gomantak Ayurveda Mahavidyalaya & Research Centre </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Devi Sharvani Education Society's V. M. Salgaocar College of Law </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Don Bosco College of Agriculture </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Ganpat Parsekar College of Education </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Goa College of Architecture </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Goa College of Art </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Goa College of Engineering </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Goa College of Music </button>"],
+ 
+    },
+
+    {
+        "tag": "recognised_institution",
+        "patterns": ["recognised institution","recognised university goa instituition"],
+        "responses": ["Click on colleges to know more: \
+        <br /> <button type='button' onClick='buttonAction(this) '> CSIR - National Institute of Oceanography </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Directorate of Archives and Archaelogy, Government of Goa </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Fishery Survey of India </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> ICAR - Central Coastal Agricultural Research Institute </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> ICMR - National Institute of Malaria Research </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> National Centre for Polar and Ocean Research </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Syngenta Biosciences Pvt. Ltd. </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Thomas Stephen's Konknni Kendr </button> \
+        <br /> <button type='button' onClick='buttonAction(this) '> Xavier Centre of Historical Research </button>"],
+ 
+    }
+
+
+    ]
+}
+
+general_edu_institution_intents= {"intents":[
+    
+    {
+        "tag": "carmel_college",
+        "patterns": ["college women commerce science carmel arts","carmel college"],
+        "responses": ["Phone no: <a href ='+91-832-2790714</a><a>+91-832-2790190</a>\
+        Email: <a href ='mailto:carmelcollege64@gmail.com'> carmelcollege64@gmail.com </a>\
+            <br>Address: Nuvem, Salcete - 403604 Goa\
+                <br>Principlal: Dr. (Sr.) M. Aradhana A.C.\
+                    <br>visit college site here <a href='http://carmelcollegegoa.org/' \
+                        target='_blank' > click here </a>\
+                            <br>courses offered : B.A. Hindi / B.A. Political Science / B.Sc. Physics / M.Sc. Food Technology / B.A. Konkani / B.A. History / B.Sc. Chemistry / Bachelor of Commerce (B.Com.) / B.Sc. Botany / B.A. English / B.Sc. Biotechnology / M.A. Counseling Psychology / B.A. Economics / B.Sc. Zoology / B.A. Psychology / B.A. History (Hons.)"],
+    },
+
+    {
+        "tag": "cuncolim_edu_college",
+        "patterns": ["educational college cuncolim societys commerce arts","educational society college cuncolim"],
+        "responses": ["Phone no: <a href ='+91-832-2865210</a>\
+        <br>Email: <a href ='mailto:cescac21@yahoo.com'> cescac21@yahoo.com </a>\
+            <br>Address: Shri Pio F. Lawrence Complex, Cuncolim, Salcete - 403 703 Goa\
+                <br>Principlal: Dr. Sanjay P. Sawant Dessai\
+                    <br>visit college site here <a href='http://www.cescollege.ac.in/' \
+                        target='_blank' > click here </a>\
+                            <br>courses offered : B.A. Political Science / B.A. Sociology / B.A. Hindi / Bachelor of Commerce (B.Com.) / B.A. History / B.A. Geography / B.A. Economics / B.A. Konkani"],
+    },
+
+    {
+        "tag": "charitiestrust_dempo_college",
+        "patterns": ["charities trust college dempo science dhempe arts","charities trust college dempo"],
+        "responses": ["Phone no: <a href ='+91-832-2462376</a><a>+91-832-2461507</a>\
+        <br>Email: <a href ='mailto:dhempe@rediffmail.com'> dhempe@rediffmail.com </a>\
+            <br>Address: P. B. No 222, Miramar, Panaji - 403001 Goa\
+                <br>Principlal: Dr. Vrinda P. S. Borkar\
+                    <br>visit college site here <a href='http://www.dhempecollege.edu.in/' \
+                        target='_blank' > click here </a>\
+                            <br>course offered : B.Sc. Mathematics / B.A. French / B.A. History (Hons.) / B.A. Hindi / B.Sc. Computer Science / B.A. Political Science / B.Sc. Physics / B.Sc. Biotechnology / B.A. Konkani / B.A. History / B.Sc. Chemistry / B.Sc. Botany / B.A. Marathi / B.A. Philosophy / B.A. English / B.Sc. Geology / B.A. Economics / B.Sc. Zoology / B.A. Psychology"],
+    },
+
+    {
+        "tag": "rosary_college",
+        "patterns": ["education college diocesan commerce society rosary arts","rosary college"],
+        "responses": ["Phone no: <a href ='+91-832-2701564</a><a>+91-832-2736864</a>(fax)\
+        <br>Email: <a href ='sarycollege.1990@gmail.com'> sarycollege.1990@gmail.com </a>\
+            <br>Address: Navelim, Salcete - 403707 Goa\
+                <br>Principlal: Rev. Dr. Simao R. Diniz (Contractual)\
+                    <br>visit college site here <a href='http://rosarycollege.org/' \
+                        target='_blank' > click here </a>\
+                            <br>courses offered : Ph.D. Economics / B.B.A. Travel & Tourism / Bachelor of Business Administration / Bachelor of Commerce (B.Com.) / B.A. English / B.A. History / B.A. Konkani / B.A. Psychology / B.A. Economics / Bachelor of Computer Application (B.C.A.) / B.A. Sociology"],
+    },
+
+    {
+        "tag": "dnyan_mandals_college",
+        "patterns": ["chetan desai mallikarjun manju college shri shree prabodhini mandals dnyan","prabodhini mandals dnyan"],
+        "responses": ["Phone no: <a href ='+91-832-2633433</a><a>+91-832-2633422</a>\
+        <br>Email: <a href ='mailto:shreemallikarjuncollege@gmail.com'> shreemallikarjuncollege@gmail.com </a>\
+            <br>Address: Delem, Canacona - Goa 403702\
+                <br>Principlal: Dr. Manoj S. Kamat\
+                    <br>visit college site here <a href='http://shreemallikarjuncollege.ac.in/' \
+                        <br>target='_blank' > click here </a>\
+                            <br>courses offered : B.A. Marathi / B.A. History / B.A. Hindi / Bachelor of Commerce (B.Com.) / B.A. Economics / B.A. Political Science / B.A. Sociology / B.A. Konkani / B.A. Geography"],
+    }
+
+    ]
+}
+
+professional_edu_institution_intents = {"intents":[
+
+    {
+        "tag": "agnel_institution",
+        "patterns": ["design technology agnel institute","agnel institute"],
+        "responses": ["Phone no: <a href ='+91-832-2268642</a><a>+91-832-2790190</a>\
+        Email: <a href ='mailto:aitdgoa@gmail.com'> aitdgoa@gmail.com </a>\
+            <br>Address: Assago, Bardez - 403507 Goa\
+                <br>Principlal: Dr. V. Mariappan\
+                    <br>visit college site here <a href='http://aitdgoa.edu.in/' \
+                        target='_blank' > click here </a>\
+                            <br>courses offered : B.E. Computer Engineering / B.E. Mechanical Engineering / B.E. Electronics & Communication Engineering"],
+    },
+
+    {
+        "tag": "ayurveda_center",
+        "patterns": ["sanskriti gomantak bharateeya ayurveda centre mahavidyalaya research prabodhinis","centre research mahavidyalaya"],
+        "responses": ["Phone no: <a href ='+91-832-2306309</a>\
+        <br>Email: <a href ='mailto:ayurgoa@rediffmail.com'> ayurgoa@rediffmail.com </a>\
+            <br>Address: Vazem, Shiroda-403 103 Goa\
+                <br>Principlal: Dr. Anura Prashant Bale\
+                    <br>visit college site here <a href='http://gamrc.org/GAMRC/' \
+                        target='_blank' > click here </a>\
+                            <br>courses offered : M.D. Ayurveda Medicine (Ayurveda Vachaspati-Kayachikitsa) / M.D. Ayurveda Pharmaceuticals (Ayurveda Vachaspati-Rasashastra and Bhaisajya Kalpana) / Bachelor of Ayurvedic Medicine and Surgery (B.A.M.S.)"],
+    },
+
+    {
+        "tag": "salgaocar_law_college",
+        "patterns": ["education societys salgaocar law college v sharvani devi","salgaocar law college"],
+        "responses": ["Phone no: <a href ='+91-832-2462376</a><a>+91-832-2461507</a>\
+        <br>Email: <a href ='mailto:dhempe@rediffmail.com'> vmscl@rediffmail.com </a>\
+            <br>Address: Miramar, Panaji- 403 001 Goa\
+                <br>Principlal: Dr. M.R.K. Prasad\
+                    <br>visit college site here <a href='http://www.vmslaw.edu.in/' \
+                        target='_blank' > click here </a>\
+                            <br>course offered : Ph.D. Law / LL.B. / LL.B.(Hons.) / L.L.M. / B.A.LL.B."],
+    },
+
+    {
+        "tag": "don_bosco_college",
+        "patterns": ["college agriculture bosco","don bosco college"],
+        "responses": ["Phone no: <a href ='+91-832-2701564</a><a>+91-832-2736864</a>(fax)\
+        <br>Email: <a href ='sarycollege.1990@gmail.com'> sarycollege.1990@gmail.com </a>\
+            <br>Address: Navelim, Salcete - 403707 Goa\
+                <br>Principlal: Rev. Dr. Simao R. Diniz (Contractual)\
+                    <br>visit college site here <a href='http://rosarycollege.org/' \
+                        target='_blank' > click here </a>\
+                            <br>courses offered : Ph.D. Economics / B.B.A. Travel & Tourism / Bachelor of Business Administration / Bachelor of Commerce (B.Com.) / B.A. English / B.A. History / B.A. Konkani / B.A. Psychology / B.A. Economics / Bachelor of Computer Application (B.C.A.) / B.A. Sociology"],
+    },
+
+    {
+        "tag": "ganpat_college",
+        "patterns": ["college ganpat parsekar education","ganpat parsekar college"],
+        "responses": ["Phone no: <a href ='+91-832-2633433</a><a>+91-832-2633422</a>\
+        <br>Email: <a href ='mailto:shreemallikarjuncollege@gmail.com'> shreemallikarjuncollege@gmail.com </a>\
+            <br>Address: Delem, Canacona - Goa 403702\
+                <br>Principlal: Dr. Manoj S. Kamat\
+                    <br>visit college site here <a href='http://shreemallikarjuncollege.ac.in/' \
+                        <br>target='_blank' > click here </a>\
+                            <br>courses offered : B.A. Marathi / B.A. History / B.A. Hindi / Bachelor of Commerce (B.Com.) / B.A. Economics / B.A. Political Science / B.A. Sociology / B.A. Konkani / B.A. Geography"],
+    }
+
+    ]
+}
+
+recognised_edu_institution_intents = {"intents":[
+
+    {
+        "tag": "oceanography_institute",
+        "patterns": ["oceanography csir institute national","oceanography institute"],
+        "responses": ["Phone no: <a href ='+91-832-2450201</a>\
+        Email: <a href ='mailto:sunil@nio.org'> sunil@nio.org </a>\
+            <br>Address: NIO, Dona Paula-Goa\
+                <br>Head of Institution: Smt. Blossom Medeira, Director\
+                    <br>visit college site here <a href='http://www.nio.org/' \
+                        target='_blank' > click here </a>"],
+    }, 
+
+    {
+        "tag": "fishery_india",
+        "patterns": ["survey india fishery","india fishery"],
+        "responses": ["Phone no: <a href ='+91-832-2520957</a>\
+        <br>Email: <a href ='mailto:fsimormugaobase@gmail.com'> fsimormugaobase@gmail.com </a>\
+            <br>Address: Opp. Microwave Towers, Bogda, Mormugao - 403803 Goa\
+                <br>Head of Institution: Shri S.K. Jaiswal (In-Charge, Zonal Director)\
+                    <br>visit college site here <a href='http://fsi.gov.in/LATEST-WB-SITE/fsi-cont-us-frm.htm' \
+                        target='_blank' > click here </a>"],
+    }   
+
+    ] 
+}
+
